1287c14046c2298794b6f654672599618f82cda3
Joe <jpkribs@outlook.com>
2025-09-03 18:58:14 -0600

    Version Bump

--- packaging/debian/build-deb.sh
+++ packaging/debian/build-deb.sh
@@ -15,7 +15,7 @@ esac
 
 echo "Detected architecture: $(uname -m) -> Go: $GO_ARCH, Debian: $DEB_ARCH"
 
-VERSION="1.1.0"
+VERSION="1.1.1"
 GO_VERSION="1.24.2"
 echo "Building FinGuard Debian package..."
 echo "Project root: $PROJECT_ROOT"
